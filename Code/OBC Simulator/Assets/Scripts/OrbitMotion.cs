453a8d7c1b9925a8fd002a622aa33d8f034324b1
lanillo <luis.felipe.anillo@usherbrooke.ca>
2018-11-15 15:49:33 -0500

    Solar system ready

---
+++ Code/OBC Simulator/Assets/Scripts/OrbitMotion.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitMotion : MonoBehaviour
+{
+
+    public Transform orbitingObject;
+    public Ellipse orbitPath;
+
+    [Range(0f, 1f)]
+    public float orbitProgress = 0f;
+    public float orbitPeriod = 3f;
+    public bool orbitActive = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Check if there is an orbiting object
+        if (orbitingObject == null)
+        {
+            orbitActive = false;
+            return;
+        }
+
+        SetOrbitingObject();
+        StartCoroutine(AnimateOrbit());
+
+	}
+
+    void SetOrbitingObject()
+    {
+        Vector2 orbitPos = orbitPath.Evaluate(orbitProgress);
+        orbitingObject.localPosition = new Vector3(orbitPos.x, 1f, orbitPos.y);
+    }
+
+    IEnumerator AnimateOrbit()
+    {
+        if (orbitPeriod < 0.5f)
+            orbitPeriod = 0.5f;
+
+        float orbitSpeed = 1f / orbitPeriod;
+        while (orbitActive)
+        {
+            orbitProgress += Time.deltaTime * orbitSpeed;
+            orbitProgress %= 1f; // reset to 1;
+            SetOrbitingObject();
+            yield return null;
+        }
+    }
+}
---
+++ Code/OBC Simulator/Assets/Scripts/Rotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    [Tooltip("Spin: Yes or No")]
+    public bool spin;
+    public float speed = 10f;
+
+    public bool clockwise = true;
+    [HideInInspector]
+    public float direction = 1f;
+    [HideInInspector]
+    public float directionChangeSpeed = 2f;
+
+    void Update()
+    {
+        if (direction < 1f)
+        {
+            direction += Time.deltaTime / (directionChangeSpeed / 2);
+        }
+
+        if (spin)
+        {
+            if (clockwise)
+            {
+                transform.Rotate(Vector3.up, (speed * direction) * Time.deltaTime);
+            }
+            else
+            {
+                transform.Rotate(-Vector3.up, (speed * direction) * Time.deltaTime);
+            }
+        }
+    }
+}
