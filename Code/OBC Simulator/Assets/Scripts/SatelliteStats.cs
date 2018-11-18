0e3a1cb76c68b03aa33cc70253ce02ead87b6b3b
lanillo <luis.felipe.anillo@usherbrooke.ca>
2018-11-18 13:56:59 -0500

    GameOver Screen

---
+++ Code/OBC Simulator/Assets/Scripts/SatelliteStats.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SatelliteStats : MonoBehaviour {
+
+    public float battery = 100f;
+    public bool commCollision = false;
+    public bool lightCollision = false;
+
+    public Text batteryText;
+
+    public float batteryDechargeRate = 0.1f;
+    public float batteryRechargeRate = 0.1f;
+
+    public GameObject GameOver;
+    public bool satelliteDead = false;
+
+    private void Start()
+    {
+        commCollision = PlayerPrefsX.GetBool("commCollision");
+        lightCollision = PlayerPrefsX.GetBool("lightCollision");
+        GameOver.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (battery <= 0f)
+        {
+            satelliteDead = true;
+            GameOver.SetActive(true);
+            batteryText.text = "";
+        }
+
+        if (satelliteDead)
+            return;
+
+        commCollision = PlayerPrefsX.GetBool("commCollision");
+        lightCollision = PlayerPrefsX.GetBool("lightCollision");
+
+        BatteryUpdate();
+
+    }
+
+    void BatteryUpdate()
+    {
+        if (!lightCollision)
+        {
+            battery -= batteryDechargeRate * Time.deltaTime;
+        }
+        else
+        {
+            battery += batteryRechargeRate * Time.deltaTime;
+        }
+
+        battery = Mathf.Clamp(battery, 0.00f, 100.0f);
+
+        batteryText.text = "Battery: " + battery.ToString("0.00") + "%";
+    }
+
+
+
+
+
+}
